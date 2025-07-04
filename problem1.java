76325495bae57099158664f32dfbe30a5da4f87c
su <smalfhaid@gmail.com>
2025-07-04 21:08:48 +0300

    Problem 1

---
+++ problem1.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+       Scanner input = new Scanner(System.in);
+       double r = input.nextDouble();
+       double v = (4.0/3) * (3.14159) * (r*r*r);
+       System.out.printf("VOLUME = %.3f",v);
+       System.out.println();
+       
+ 
+    }
+ 
+}
